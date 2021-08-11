b931fdeec7b9598d97721ce5c091b9d0dca2660f
Francivaldo Souza <francivaldo.alves@gmail.com>
2021-08-10 21:58:55 -0300

    exercicio resolvido

--- src/main/java/com/devsuperior/bds03/services/EmployeeService.java
+++ src/main/java/com/devsuperior/bds03/services/EmployeeService.java
@@ -1,12 +1,12 @@
 package com.devsuperior.bds03.services;
 
+import com.devsuperior.bds03.dto.EmployeeDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.bds03.dto.EmployeeDTO;
 import com.devsuperior.bds03.entities.Department;
 import com.devsuperior.bds03.entities.Employee;
 import com.devsuperior.bds03.repositories.EmployeeRepository;
